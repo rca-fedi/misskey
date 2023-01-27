032ed8828844f0ca7e91bd283a6ef56db4f12b74
r-ca <nem@nem.x0.to>
2023-01-27 16:00:50 +0900

    いろいろreplaceした

--- packages/client/src/components/global/MkError.vue
+++ packages/client/src/components/global/MkError.vue
@@ -1,7 +1,7 @@
 <template>
 <transition :name="$store.state.animation ? 'zoom' : ''" appear>
 	<div class="mjndxjcg">
-		<img src="https://xn--931a.moe/assets/error.jpg" class="_ghost"/>
+		<img src="$store.state.assetImageError" class="_ghost"/>
 		<p><i class="fas fa-exclamation-triangle"></i> {{ i18n.ts.somethingHappened }}</p>
 		<MkButton class="button" @click="() => $emit('retry')">{{ i18n.ts.retry }}</MkButton>
 	</div>
--- packages/client/src/pages/messaging/index.vue
+++ packages/client/src/pages/messaging/index.vue
@@ -33,7 +33,7 @@
 				</MkA>
 			</div>
 			<div v-if="!fetching && messages.length == 0" class="_fullinfo">
-				<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+				<img src="$store.state.assetImageInfo" class="_ghost"/>
 				<div>{{ $ts.noHistory }}</div>
 			</div>
 			<MkLoading v-if="fetching"/>
