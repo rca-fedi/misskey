6b0416784d43f7c02bf47fba2e0d132128ad914b
r-ca <nem@nem.x0.to>
2023-05-08 16:22:47 +0900

    appinfoが見れるようになった

--- packages/backend/src/servers/v12c/miauth-client/script.js
+++ packages/backend/src/servers/v12c/miauth-client/script.js
@@ -1,3 +1,4 @@
+const { isArrayTypeNode } = require("typescript");
 
 const api = location.origin + '/api/miauth/gen-token';
 
@@ -5,22 +6,28 @@ function post() {
 	const xhr = new XMLHttpRequest();
 	const input = document.getElementById('input');
 	const apikey = input.value;
-	const permissions__ = permissions.split(',')
+	const permissions__ = permissions_.split(',')
 	const data = {
 		"i": apikey,
-		"session": token,
-		"name": "hoge(HARD)", //TODO: ハードコーディングやめる
-		"permission": permissions__,
+		"session": token_,
+		"name": name_,
+		"permission": permissions__, //TODO: 条件分岐して存在する値だけ送るようにする
+		// "iconUrl": icon_,
 	};
 	//DEBUG
 	console.log(data);
 	console.log(JSON.stringify(data));
 	console.log(api);
 	xhr.onload = function() {
-		console.log(xhr.responseText);
+		console.log(xhr.responseText); //debug
+		if (callback_ !== null) {
+			resp = JSON.parse(xhr.responseText);
+			console.log(callback_ + '?token=' + resp.token);
+			location.href = callback_ + '?token=' + resp.token;
+		}
 	};
 	xhr.onerror = function() {
-		// エラー処理する
+		alert('An error occurred during the transaction. \n ' + xhr.status + ' : ' + xhr.statusText);
 	};
 	xhr.open('POST', api,true);
 	xhr.setRequestHeader('Content-Type', 'application/json');
