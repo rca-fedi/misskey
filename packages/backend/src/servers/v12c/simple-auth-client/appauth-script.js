a44ecb638d578e6754995b7e395110e3e771cbbf
r-ca <nem@nem.x0.to>
2023-05-14 18:35:30 +0900

    コールバック処理書いてみた

--- packages/backend/src/servers/v12c/simple-auth-client/appauth-script.js
+++ packages/backend/src/servers/v12c/simple-auth-client/appauth-script.js
@@ -1,23 +1,52 @@
 
-const endpoint = location.origin + '/api/auth/accept';
+const origin = location.origin;
+let callback = null;
+fetchMeta();
+
+function fetchMeta() {
+	const url = origin + '/api/auth/session/show';
+
+	const data_meta = {
+		"token": token_,
+	}
+
+	xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		console.log(xhr.responseText);
+		const res = JSON.parse(xhr.responseText);
+		callback = res.app.callbackUrl;
+	}
+	xhr.onerror = function() {
+		// エラー処理する
+	}
+	xhr.open('POST', origin + '/api/auth/session/show', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(data_meta));
+
+}
+
 
 function post() {
 	const input = document.getElementById('input');
 	const apikey = input.value;
 
-	const data = {
+	const data_accept = {
 		"i": apikey,
 		"token": token_,
 	};	
 
 	const xhr = new XMLHttpRequest();
 	xhr.onload = function() {
-		// コールバックとか書く
+		console.log(xhr.responseText);
+		const res = JSON.parse(xhr.responseText);
+		if (callback) {
+			location.href = callback + '?token=' + res.accessToken;
+		}
 	}
 	xhr.onerror = function() {
 		// エラー処理とか書く
 	}
-	xhr.open('POST', endpoint, true);
+	xhr.open('POST', origin + '/api/auth/accept', true);
 	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify(data));
+	xhr.send(JSON.stringify(data_accept));
 }
